0e3dcd48e4ce5fd354d898c410aa5e51f360c163
grace <grace.c.youngblood@gmail.com>
2021-03-21 18:48:13 -0400

    Basic second spec, pending.

--- spec/lib/processors_spec.rb
+++ spec/lib/processors_spec.rb
@@ -2,7 +2,7 @@ require "processors/congress/xml"
 require "processors/congress/txt"
 
 RSpec.describe "congressional bill processors" do
-  describe "117th HR 1; 'For the People'" do
+  describe "on 117th HR 1; 'For the People'" do
     it "is processed equally, whether as XML or TXT" do
       xml_processor = Processor::Congress::XML.new \
         File.read \
@@ -15,6 +15,18 @@ RSpec.describe "congressional bill processors" do
       processed_b = txt_processor.process
 
       expect(processed_a.to_s).to eq processed_b.to_s
+    end
+
+    it "Pulls ten titles, using roman numerals" do
+      xml_processor = Processor::Congress::XML.new \
+        File.read \
+        File.expand_path( "../../uploads/BILLS-117hr1eh.txt", __FILE__)
+
+      processed = xml_processor.process
+
+      expect(processed.measures("title")).to include(
+        ["X", "Presidential and Vice Presidential tax transparency".upcase]
+      )
     end
   end
 end
