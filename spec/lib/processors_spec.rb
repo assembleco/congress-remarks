b11a20115210739bf1ad6d7c865ca3015b6b3eb3
grace <grace.c.youngblood@gmail.com>
2021-03-21 18:20:10 -0400

    Add a bare-bones spec harness for our processors.

---
+++ spec/lib/processors_spec.rb
@@ -0,0 +1,10 @@
+require "processors/congress/xml"
+require "processors/congress/txt"
+
+RSpec.describe "congressional bill processors" do
+  describe "117th HR 1; 'For the People'" do
+    pending "is processed equally, whether as XML or TXT" do
+
+    end
+  end
+end
