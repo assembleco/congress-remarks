e785ec8eb1c344b8bb105f3016b64cb67041aa0f
grace <grace.c.youngblood@gmail.com>
2021-03-21 18:48:23 -0400

    Expand second spec using full example.

--- spec/lib/processors_spec.rb
+++ spec/lib/processors_spec.rb
@@ -17,16 +17,25 @@ RSpec.describe "congressional bill processors" do
       expect(processed_a.to_s).to eq processed_b.to_s
     end
 
-    it "Pulls ten titles, using roman numerals" do
+    pending "Pulls ten titles, labeled using roman numerals" do
       xml_processor = Processor::Congress::XML.new \
         File.read \
         File.expand_path( "../../uploads/BILLS-117hr1eh.txt", __FILE__)
 
       processed = xml_processor.process
 
-      expect(processed.measures("title")).to include(
-        ["X", "Presidential and Vice Presidential tax transparency".upcase]
-      )
+      expect(processed.measures("title").map{|t| [t.label, t.heading]}).to eq([
+        ["I", "ELECTION ACCESS"],
+        ["II", "ELECTION INTEGRITY"],
+        ["III", "ELECTION SECURITY"],
+        ["IV", "CAMPAIGN FINANCE TRANSPARENCY"],
+        ["V", "SMALL DOLLAR FINANCING OF CONGRESSIONAL ELECTION CAMPAIGNS"],
+        ["VI", "CAMPAIGN FINANCE OVERSIGHT"],
+        ["VII", "ETHICAL STANDARDS"],
+        ["VIII", "ETHICAL REFORMS FOR THE PRESIDENT, VICE PRESIDENT, AND FEDERAL OFFICERS AND EMPLOYEES"],
+        ["IX", "CONGRESSIONAL ETHICS REFORM"],
+        ["X", "PRESIDENTIAL AND VICE pRESIDENTIAL TAX TRANSPARENCY"],
+      ])
     end
   end
 end
