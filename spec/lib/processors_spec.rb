6500ddd721d3783c16afba2658afbad2776701cd
grace <grace.c.youngblood@gmail.com>
2021-03-22 18:05:32 -0400

    Add a pending spec; pull subtitles,parts,sections

--- spec/lib/processors_spec.rb
+++ spec/lib/processors_spec.rb
@@ -56,5 +56,21 @@ RSpec.describe "congressional bill processors" do
         ["III", "Election Security"],
       ])
     end
+
+    pending "pulls parts and sections" do
+      xml_processor = Processor::Congress::XML.new \
+        File.read \
+        File.expand_path( "../../uploads/BILLS-117hr1eh.xml", __FILE__)
+
+      processed = xml_processor.process
+
+      sec_1001 = processed.
+        measures(:subtitle)[0].
+        measures(:part)[0].
+        measures(:section)[0]
+
+      expect(sec_1001.label).to eq("1001")
+      expect(sec_1001.heading).to eq("Requiring Availability of Internet for Voter Registration")
+    end
   end
 end
