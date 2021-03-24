9d788af4b77f20d3c1c790c3fd36e18c952bcf3c
grace <grace.c.youngblood@gmail.com>
2021-03-24 13:43:37 -0400

    [broken] begin rehashing a key-based lookup

--- lib/processors/congress/xml.rb
+++ lib/processors/congress/xml.rb
@@ -23,7 +23,6 @@ module Processor
             node.replace("&lquot;#{node.text}&rquot;")
           end
 
-          upper = node.ancestors[0]
           recognized_measures = %w[
           division
           title
@@ -38,26 +37,20 @@ module Processor
           subitem
           ]
 
-          unless \
-              recognized_measures.include?(node.name) &&
-              recognized_measures.include?(upper.name)
-            next
-          end
+          next unless recognized_measures.include?(node.name)
+
           measure = process_measure(node)
+          node.replace("{place \"#{measure.key}\"}")
 
-          lookup[upper] ||= Measure.new(
-            upper.name.to_sym,
-            upper.search("enum")[0].text,
-            (upper.search("header")[0].text rescue nil),
-            nil,
-            [],
-            upper.attr("id"),
-          )
+          lookup[measure.key] ||= measure
 
-          lookup[upper].add_submeasure(lookup[node] || measure)
+          submeasure_keys = node.text.scan(/\{place +\"([A-H0-9]+)\"\}/)
+          lookup[measure.key].add_submeasure(measure)
         end
 
-        division_measures = body.search("division").map{|dn| lookup[dn] }
+        division_measures = body.
+          search("division").
+          map{|dn| lookup[dn.attr('id')] }
 
         # puts division_measures.map(&:submeasures)
         Measure.new(
