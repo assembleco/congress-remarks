828b08988fc83b5c5493070dac8de526d09db7e2
grace <grace.c.youngblood@gmail.com>
2021-03-25 13:07:16 -0400

    clean up prop check.

--- src/code.js
+++ src/code.js
@@ -3,7 +3,7 @@ import { observer } from "mobx-react"
 
 const Code = observer(({ source }) => (
   <div>
-    <h1>{source && source.key}</h1>
+    <h1>{source.key}</h1>
     <div>
       <h2>{source.measure.label}: {source.measure.heading}</h2>
       <p>{source.measure.source}</p>
