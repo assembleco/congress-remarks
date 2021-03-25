1c0d8985c27e5a083870ed7547f6a8bd39dafad4
grace <grace.c.youngblood@gmail.com>
2021-03-25 13:03:33 -0400

    Render bill measures as a JSON response.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,6 +1,14 @@
+require "processors/congress/xml"
+
 class BillsController < ApplicationController
   def show
     key = params[:id]
-    render json: { key: key }
+
+    xml_processor = Processor::Congress::XML.new \
+      File.read \
+      Rails.root.join("spec/uploads/BILLS-117hr1eh.xml")
+    processed = xml_processor.process
+
+    render json: { key: key, source: processed.as_json }
   end
 end
