3f70960024fd20de54ed1be550bd7cc2c3923053
grace <grace.c.youngblood@gmail.com>
2021-03-25 16:19:43 -0400

    Use solarized color scheme on border lines.
    See https://ethanschoonover.com/solarized/

--- src/code.js
+++ src/code.js
@@ -5,11 +5,11 @@ import styled from "styled-components"
 const Code = observer(({ source }) => (
   <Page>
     <h1>{source.key}</h1>
-    <Measure {...source.measure} />
+    <Measure {...source.measure} level={0} />
   </Page>
 ))
 
-var Measure = observer(({ marker, label, heading, source, submeasures }) => {
+var Measure = observer(({ marker, label, heading, source, submeasures, level }) => {
   var body = source || ''
 
   var matches = body.matchAll(/\{place +\"([A-H0-9]+)\"\}/g)
@@ -22,7 +22,7 @@ var Measure = observer(({ marker, label, heading, source, submeasures }) => {
     index = code.value.index
 
     var measure = submeasures.filter(x => x.key == code.value[1])[0]
-    children.push(<Measure {...measure} />)
+    children.push(<Measure {...measure} level={level + 1} />)
     index += code.value[0].length
 
     code = matches.next()
@@ -30,7 +30,7 @@ var Measure = observer(({ marker, label, heading, source, submeasures }) => {
   children.push(body.slice(index))
 
   return (
-    <Borderline>
+    <Borderline level={level} >
       <h3>{label}: {heading}</h3>
       {children}
     </Borderline>
@@ -45,9 +45,20 @@ overflow-x: hidden;
 `
 
 var Borderline = styled.div`
-border-left: 4px solid grey;
 padding-left: 12px;
 white-space: pre-wrap;
+border-left: 4px solid ${({ level }) => (
+[
+"#b58900",
+"#cb4b16",
+"#dc322f",
+"#d33682",
+"#6c71c4",
+"#268bd2",
+"#2aa198",
+"#859900",
+][level] || 'grey'
+)};
 `
 
 export default Code
