f54226e57f3bae960a88dc4f00c0621f56df66fc
grace <grace.c.youngblood@gmail.com>
2021-03-30 18:39:12 -0400

    Basic headlines for general use and sponsorship.

--- src/homepage.js
+++ src/homepage.js
@@ -5,7 +5,16 @@ import styled from "styled-components"
 var Homepage = ({ children }) => (
   <Grid>
     <h1>"For the People" Act</h1>
-    <span>Sponsorship...</span>
+    <p>
+      Leave your remarks on House Resoultion # 1,
+      <br/>the "For the People" act.
+      <br/>Remind Congress – our laws should also
+      <br/>be made <b>*by the people*</b>.
+
+      <h4>Seeking Sponsorship</h4>
+      Help us upgrade and change congressional bills
+      <br/>using a social media-based approach.
+    </p>
     {children}
   </Grid>
 )
@@ -14,6 +23,7 @@ var Grid = styled.div`
 display: grid;
 grid-template-columns: auto 1fr;
 grid-column-gap: 4rem;
+font-family: "Ruluko";
 `
 
 export default Homepage
