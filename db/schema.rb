7139e16467edb0bdfe69168791b0b3f177604e95
grace <grace.c.youngblood@gmail.com>
2021-04-03 18:28:11 -0400

    Make `Session` record model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_222032) do
+ActiveRecord::Schema.define(version: 2021_04_03_222643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,15 @@ ActiveRecord::Schema.define(version: 2021_04_03_222032) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "person_id", null: false
+    t.datetime "claimed"
+    t.datetime "expires"
+    t.uuid "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["person_id"], name: "index_sessions_on_person_id"
+  end
+
+  add_foreign_key "sessions", "people"
 end
