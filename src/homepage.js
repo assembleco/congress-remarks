98616719ee66c3900f406046a9e3d4344a83c2da
grace <grace.c.youngblood@gmail.com>
2021-04-04 15:18:06 -0400

    Rearrange using `@media print` for press-readiness

--- src/homepage.js
+++ src/homepage.js
@@ -5,24 +5,51 @@ import styled from "styled-components"
 var Homepage = ({ children }) => (
   <Grid>
     {children}
-    <p>
+
+    <Bylines>
       <h2>H.R. 1: "For the People"</h2>
-      Leave your remarks on House Resoultion # 1.
-      <br/>Remind Congress – our laws should also
-      <br/>be made <b>*by the people*</b>.
+      <p>
+        Leave your remarks on House Resoultion # 1.
+        <br/>Remind Congress – our laws should also
+        <br/>be made <b>*by the people*</b>.
+      </p>
 
       <h4>Seeking Sponsorship</h4>
-      Help us upgrade and change congressional bills
-      <br/>by sourcing ideas and remarks on social media.
-    </p>
+      <p>
+        Help us upgrade and change congressional bills
+        <br/>by sourcing ideas and remarks on social media.
+        <br/>
+        <Press>
+          Please see <a href="https://assembled.app">assembled.app</a>, online.
+        </Press>
+      </p>
+    </Bylines>
   </Grid>
 )
+
+var Bylines = styled.div`
+`
 
 var Grid = styled.div`
 display: grid;
 grid-template-columns: auto 1fr;
 grid-column-gap: 4rem;
 font-family: "Ruluko";
+
+@media print {
+  grid-template-columns: 1fr;
+  ${Bylines} {
+    grid-row: 1;
+  }
+}
+`
+
+var Press = styled.span`
+display: none;
+
+@media print {
+  display: inline;
+}
 `
 
 export default Homepage
