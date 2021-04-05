0df7d564bc0243efed3f7028974310878a669f45
grace <grace.c.youngblood@gmail.com>
2021-04-05 09:43:36 -0400

    [broken] Begin reshaping `/remarks` handler.

--- app/controllers/remarks_controller.rb
+++ app/controllers/remarks_controller.rb
@@ -1,20 +1,7 @@
 class RemarksController < ApplicationController
   def index
-    render json: [
-      {
-        place: "HC5BBEC22F049435CB96653F6F225CFE8",
-        body: <<-END,
-This Table of Contents has been suppressed
-because it is too long.
-
-Please click "collapse all measures" to see
-only the code's measure headings.
-END
-        person: {
-          handle: "c4lliope",
-          badges: ["programmer"],
-        },
-      },
-    ]
+    render json: Remark.all.includes(:person).map do |r|
+      r.slice(:place, :body).merge(person: r.person.slice(:handle))
+    end
   end
 end
---
+++ spec/requests/remarks_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Remarks address" do
+  it "renders remarks" do
+    remark = create :remark
+    get "/remarks"
+
+    expect(JSON.parse(response.body)).to eq([
+      {
+        place: remark.place,
+        body: remark.body,
+        person: {
+          handle: remark.person.handle,
+        }
+      }
+    ])
+  end
+end
