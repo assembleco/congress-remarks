6a13d310dee4d53ccb9b89b7fc0f4b7de1e7a23b
grace <grace.c.youngblood@gmail.com>
2021-04-05 09:54:07 -0400

    [repaired] Hook up `/remarks` address properly.

--- app/controllers/remarks_controller.rb
+++ app/controllers/remarks_controller.rb
@@ -1,7 +1,10 @@
 class RemarksController < ApplicationController
   def index
-    render json: Remark.all.includes(:person).map do |r|
-      r.slice(:place, :body).merge(person: r.person.slice(:handle))
+    remarks = Remark.all.includes(:person).map do |r|
+      r.slice(:place, :body).
+        merge(person: r.person.slice(:handle).merge(badges: []))
     end
+
+    render json: remarks
   end
 end
--- spec/requests/remarks_spec.rb
+++ spec/requests/remarks_spec.rb
@@ -5,28 +5,13 @@ RSpec.describe "Remarks address" do
     remark = create :remark
     get "/remarks"
 
-    expect(JSON.parse(
-      Remark.all.includes(:person).map do |r|
-        r.slice(:place, :body).merge(person: r.person.slice(:handle))
-      end
-    .to_json)
-    ).to eq([
-      {
-        "place" => remark.place,
-        "body" => remark.body,
-        "person" => {
-          "handle" => remark.person.handle,
-        }
-      }
-    ])
-
-
     expect(JSON.parse(response.body)).to eq([
       {
         "place" => remark.place,
         "body" => remark.body,
         "person" => {
           "handle" => remark.person.handle,
+          "badges" => [],
         }
       }
     ])
