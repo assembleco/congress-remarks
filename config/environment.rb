e52825959d717e88b7413da380e2632184c8cc65
grace <grace.c.youngblood@gmail.com>
2021-04-05 14:22:21 -0400

    Change email config, use Sendgrid.

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,16 @@
 require_relative 'application'
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
+  :domain => ENV["APPLICATION_HOST"],
+  :address => 'smtp.sendgrid.net',
+  :port => "465",
+  :authentication => :plain,
+  :enable_starttls_auto => true,
+  :openssl_verify_mode    => "none",
+  ssl: true,
+  tls: true,
+}
+
 Rails.application.initialize!
